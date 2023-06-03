4583becd3c1bbe8187b4786f34980e7a71a969fe
Jose Jaime Come <josejaimecome@gmail.com>
2023-06-03 20:31:43 +0200

    Changes made

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,22 @@ import PropTypes from 'prop-types'
 
 const Header = ({ title }) => {
   return (
-    <header className='header'>
-      <h1>{title}</h1>
-    </header>
+    <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow header">
+    <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="/">{ title }</a>
+    <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="navbar-nav">
+      <div className="nav-item">
+          <table>
+              <tr>
+                  <td><a class="nav-link px-3" href="#">{ "Test" }</a></td>
+                  <td><a class="nav-link px-3" href="/logout">Sign out</a></td>
+              </tr>
+          </table> 
+      </div>
+    </div>
+  </header>
   )
 }
 
